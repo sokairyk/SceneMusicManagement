2591f6be103fb96f6b790409a1b173ca16c4979f
Kyriakos Filippakos <k.filippakos@clmsuk.com>
2019-09-05 16:33:19 +0300

    Initial Commit

---
+++ CollectionManagementLib/IntegrityChecker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionManagementLib
+{
+    public static class CRC32
+    {
+        private const UInt32 DefaultPolynomial = 0xedb88320u;
+        private const UInt32 DefaultSeed = 0xffffffffu;
+        private static UInt32[] DefaultTable;
+
+        static CRC32()
+        {
+            DefaultTable = InitializeTable(DefaultPolynomial);
+        }
+
+        private static UInt32[] InitializeTable(UInt32 polynomial)
+        {
+            var createTable = new UInt32[256];
+            for (var i = 0; i < 256; i++)
+            {
+                var entry = (UInt32) i;
+                for (var j = 0; j < 8; j++)
+                    if ((entry & 1) == 1)
+                        entry = (entry >> 1) ^ polynomial;
+                    else
+                        entry = entry >> 1;
+                createTable[i] = entry;
+            }
+
+            return createTable;
+        }
+
+        private static UInt32 CalculateHash(IList<byte> buffer)
+        {
+            var start = 0;
+            var size = buffer.Count;
+            var hash = DefaultSeed;
+            for (var i = start; i < start + size; i++)
+                hash = (hash >> 8) ^ DefaultTable[buffer[i] ^ hash & 0xff];
+            return hash;
+        }
+        
+        public static UInt32 Compute(byte[] buffer)
+        {
+            return ~CalculateHash(buffer);
+        }
+    }
+}
---
+++ ConsoleTesting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Calculating SFV...");
+
+            var songBytes = File.ReadAllBytes(@"D:\Personal\Music\Artists\A Flock of Seagulls\A_Flock_Of_Seagulls_With_The_Prague_Philharmonic_Orchestra-Ascension-CD-2018-D2H\01-a_flock_of_seagulls_with_the_prague_philharmonic_orchestra-i_ran.mp3");
+            var result = CollectionManagementLib.CRC32.Compute(songBytes);
+            
+            Console.WriteLine("{0:X}", result);
+            
+            //CollectionScanner.GenerateStructure(@"D:\Personal\Music\Artists\A Flock of Seagulls\");
+        }
+    }
+}
